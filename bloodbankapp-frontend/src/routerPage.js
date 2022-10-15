aaa5395f6b7517f3195a1e722b77ad52ce31940e
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-10-15 20:04:28 +0100

    Set up linters

--- bloodbankapp-frontend/src/routerPage.js
+++ bloodbankapp-frontend/src/routerPage.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import ForgotPassword from "./pages/auth/forgotPassword";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-import ResetPassword from "./pages/auth/resetPassword";
-import LandingPage from "./pages/landingPage/landingPage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ForgotPassword from './pages/auth/forgotPassword';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import ResetPassword from './pages/auth/resetPassword';
+import LandingPage from './pages/landingPage/landingPage';
 
 export default function RouterPage() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/resetpassword" element={<ResetPassword />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
     </>
   );
 }
