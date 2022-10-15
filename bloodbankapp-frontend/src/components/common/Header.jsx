24479f4c8acd5f9ead2f0ba1e807d3ecca4ca7f0
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-10-15 21:49:39 +0100

    Add header component to the home page

---
+++ bloodbankapp-frontend/src/components/common/Header.jsx
@@ -0,0 +1,41 @@
+import { Link, NavLink } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import NavItems from './NavItems';
+
+const Header = () => {
+  const data = NavItems;
+
+  const navLinksStyles = ({ isActive }) => ({
+    borderBottom: isActive ? '4px solid #FFC01D' : 'none',
+  });
+
+  return (
+    <header className="">
+      <div className="bg-[#f00] p-10">
+        <nav className="p-5">
+          {data.map((navItem) => (
+            <div key={uuidv4()} className="flex gap-5 items-center relative">
+              <Link to={navItem.link[0]} className="flex gap-2 items-center">
+                <img src={navItem.logo} alt={navItem.alt} />
+                <h4>{navItem.title[0]}</h4>
+              </Link>
+              <ul className="flex gap-5 items-center w-[67%] m-auto">
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[1]} style={navLinksStyles}>{navItem.title[1]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[2]} style={navLinksStyles}>{navItem.title[2]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[3]} style={navLinksStyles}>{navItem.title[3]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[4]} style={navLinksStyles}>{navItem.title[4]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[5]} style={navLinksStyles}>{navItem.title[5]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[6]} style={navLinksStyles}>{navItem.title[6]}</NavLink></li>
+                <li className="hover:text-[#FFC01D]"><NavLink to={navItem.link[7]} style={navLinksStyles}>{navItem.title[7]}</NavLink></li>
+              </ul>
+
+            </div>
+
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ bloodbankapp-frontend/src/components/common/NavItems.js
@@ -0,0 +1,28 @@
+import siteLogo from '../../assets/images/common/logoNew.png';
+
+const NavItems = [
+  {
+    logo: siteLogo,
+    alt: 'Prudence Concept Logo',
+    link: [
+      '/',
+      '/donor',
+      '/patient',
+      '/dashboard',
+      '/login',
+      '/register',
+      '/blogs',
+      '/contact-us',
+    ],
+    title: [
+      'Blood Bank App',
+      'Donor',
+      'Patient',
+      'Profile',
+      'Login',
+      'Signup',
+    ],
+  },
+];
+
+export default NavItems;
